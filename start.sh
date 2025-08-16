fa841aeec78fbac1ebbcd78e82b4fa1df692d12c
David Magalhães <speeddragon@gmail.com>
2025-08-16 11:07:12 +0100

    Delete past configurations

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash 
 echo "Provider: ${PROVIDER}"
 echo "Proxy Port: ${PROXY_PORT}"
+# Delete past configurations
+rm -rf /root/.nym
+# Configure NYM
 ./nym-socks5-client init --id docs-example --use-reply-surbs true --provider $PROVIDER --port $PROXY_PORT --host 0.0.0.0
+# Start NYM
 ./nym-socks5-client run --id docs-example
